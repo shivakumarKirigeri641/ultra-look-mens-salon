573c1fe4b17a0225ed3f7d24d8d6a60129bb8de6
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-04-30 23:04:47 +0530

    feat: feed for staff and generic done

--- src/routers/servicesRouter.js
+++ src/routers/servicesRouter.js
@@ -1,12 +1,48 @@
 const express = require('express');
 const checkAuthentication = require('../middleware/checkAuthentication');
 const JobList = require("../models/jobList");
-const ServiceElements = require("../models/jobList");
-
+const ServicesList = require('../models/servicesList');
 const servicesRouter = express.Router();
 
+
+//staff - feed
+servicesRouter.get('/staff/feed', checkAuthentication, async(req, res)=>{
+    try{
+        let servcieslist = await ServicesList.find({});
+        servcieslist = servcieslist.map(x=>({
+            _id:x._id,
+            serviceName:x.serviceName,
+            createdAt:x.createdAt,
+            updatedAt:x.updatedAt,
+            price:x.prices[x.prices.length-1].price
+        }));
+        res.status(200).json({status:'Ok', message:'Data fetched successfully', data:{loggedInUser:{firstName:req.userdata.firstName, lastName:req.userdata.lastName}, servcieslist}})
+    }
+    catch(err){
+        res.status(401).json({status:'Failed', message:err.message});
+    }
+});
+
+//generic - feed
+servicesRouter.get('/feed', async(req, res)=>{
+    try{
+        let servcieslist = await ServicesList.find({});
+        servcieslist = servcieslist.map(x=>({
+            _id:x._id,
+            serviceName:x.serviceName,
+            createdAt:x.createdAt,
+            updatedAt:x.updatedAt,
+            price:x.prices[x.prices.length-1].price
+        }));
+        res.status(200).json({status:'Ok', message:'Data fetched successfully', data:servcieslist});
+    }
+    catch(err){
+        res.status(401).json({status:'Failed', message:err.message});
+    }
+});
+
 //add/update the jobs based on staff
-servicesRouter.post('/staff/updateservices', checkAuthentication, async(req, res)=>{
+servicesRouter.patch('/staff/updateservices', checkAuthentication, async(req, res)=>{
     try{
 
         const arrayofserviceids = req.body.serviceIDs;
