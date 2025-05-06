6d390bb3ebb3cd90436aaa92103e30f8bab18d4b
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-05-06 07:40:49 +0530

    task: included fetch of comboservices

--- src/routers/servicesRouter.js
+++ src/routers/servicesRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const checkAuthentication = require('../middleware/checkAuthentication');
 const JobList = require("../models/jobList");
 const ServicesList = require('../models/servicesList');
+const ComboLists = require('../models/comboLists');
 const servicesRouter = express.Router();
 
 
@@ -9,6 +10,7 @@ const servicesRouter = express.Router();
 servicesRouter.get('/staff/feed', checkAuthentication, async(req, res)=>{
     try{
         let servcieslist = await ServicesList.find({});
+        let comboserviceslist = await ComboLists.find({}).populate('serviceID', 'serviceName');
         servcieslist = servcieslist.map(x=>({
             _id:x._id,
             serviceName:x.serviceName,
@@ -16,7 +18,13 @@ servicesRouter.get('/staff/feed', checkAuthentication, async(req, res)=>{
             updatedAt:x.updatedAt,
             price:x.prices[x.prices.length-1].price
         }));
-        res.status(200).json({status:'Ok', message:'Data fetched successfully', data:{loggedInUser:{firstName:req.userdata.firstName, lastName:req.userdata.lastName}, servcieslist}})
+        res.status(200).json({status:'Ok', message:'Data fetched successfully', 
+            data:{
+                loggedInUser:
+                {firstName:req.userdata.firstName, lastName:req.userdata.lastName},
+                servcieslist,
+                comboserviceslist
+                }})
     }
     catch(err){
         res.status(401).json({status:'Failed', message:err.message});
@@ -33,8 +41,8 @@ servicesRouter.get('/feed', async(req, res)=>{
             createdAt:x.createdAt,
             updatedAt:x.updatedAt,
             price:x.prices[x.prices.length-1].price
-        }));
-        res.status(200).json({status:'Ok', message:'Data fetched successfully', data:servcieslist});
+        }));        
+        res.status(200).json({status:'Ok', message:'Data fetched successfully', standardServiceData:servcieslist, comboServiceData:comboserviceslist});
     }
     catch(err){
         res.status(401).json({status:'Failed', message:err.message});
